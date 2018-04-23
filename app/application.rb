cdbceabac069f70c732b05c223764902671388ce
Petya Djanovska <petya.djanovska@gmail.com>
2018-04-23 16:11:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
       resp = Rack::Response.new
        if Time.now.getlocal.hour.between?(12, 24)
-         resp.write "Good afternoon!"
+         resp.write "Good Afternoon!"
        else
          resp.write "Good Morning!"
        end
